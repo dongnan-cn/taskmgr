918f23bef83d6a3cd4ffe947b2a066ff8bed4d68
dongdongbing <dongnan.fr@gmail.com>
2018-10-21 23:56:25 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CoreModule } from './core/core.module'
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
