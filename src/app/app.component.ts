030d62a81f7e4c26fc09c52bd6b2f1c3215f0689
dongdongbing <dongnan.fr@gmail.com>
2018-11-15 06:33:05 +0100

    缓动函数，关键帧

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { OverlayContainer } from '@angular/cdk/overlay';
+import { keyframes, trigger, state, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger(
+      'square',
+      [
+        state('green', style({ 'background-color': 'green', 'height': '100px', 'transform': 'translateY(100%)' })),
+        state('red', style({ 'background-color': 'red', 'height': '100px', 'transform': 'translateY(-100%)' })),
+        // 缓动函数
+        transition('green => red', animate('.8s ease-in')),
+        transition('red => green', animate(5000, keyframes([
+          style({transform:'translateY(-100%)'}),
+          style({transform:'translateY(-98%)'}),
+          style({transform:'translateY(-95%)'}),
+          style({transform:'translateY(-50%)'}),
+          style({transform:'translateY(-20%)'}),
+          style({transform:'translateY(-15%)'}),
+          style({transform:'translateY(-10%)'}),
+        ]))),
+      ])
+  ]
 })
 export class AppComponent {
-  title = 'taskmgr';
+  squareState: string
   useDarkTheme = false
 
   constructor(private oc: OverlayContainer) {
@@ -21,5 +41,9 @@ export class AppComponent {
     } else {
       this.oc.getContainerElement().classList.remove('myapp-dark-theme');
     }
+  }
+
+  onClick() {
+    this.squareState = this.squareState === 'red' ? 'green' : 'red'
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { MatSidenavModule } from '@angular/material';
-
 import { CoreModule } from './core/core.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -22,9 +21,10 @@ import { TaskModule } from './task/task.module';
     LoginModule,
     MatSidenavModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
+    
     ProjectModule,
-    TaskModule
+    TaskModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
