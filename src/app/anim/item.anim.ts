ec5dd7b35028a0de270014c7f989bd0bfa787a07
dongdongbing <dongnan.fr@gmail.com>
2018-11-15 07:14:10 +0100

    bind animation to component
    bind animation to component:
    bind to the component itself (usable anywhere)
    bind to the senario of usage (usable only for specific scenario)

---
+++ src/app/anim/item.anim.ts
@@ -0,0 +1,9 @@
+import { trigger, state, transition, style, animate } from '@angular/animations';
+
+export const itemAnim = trigger('item', [
+    state('out', style({ 'border-left-width': '3px' })),
+    state('hover', style({ 'border-left-width': '8px' })),
+
+    transition('out=>hover', animate('100ms ease-in')),
+    transition('hover=>out', animate('100ms ease-out'))
+])
--- src/app/task/task-item/task-item.component.ts
+++ src/app/task/task-item/task-item.component.ts
@@ -1,17 +1,30 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, HostBinding, HostListener } from '@angular/core';
+import { itemAnim } from 'src/app/anim/item.anim';
 
 
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
-  styleUrls: ['./task-item.component.scss']
+  styleUrls: ['./task-item.component.scss'],
+  animations: [
+    itemAnim
+  ]
 })
 export class TaskItemComponent implements OnInit {
   @Input() item
   @Input() avatar
   @Output() taskClick = new EventEmitter<void>()
-  constructor() { }
+  widerPriority = 'out'
 
+  constructor() { }
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    this.widerPriority = 'hover'
+  }
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    this.widerPriority = 'out'
+  }
   ngOnInit() {
     this.avatar = this.item.owner ? this.item.owner.avatar : 'unassigned'
   }
